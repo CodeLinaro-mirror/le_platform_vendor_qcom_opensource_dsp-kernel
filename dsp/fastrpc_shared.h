352708c1281b2a9f2524315ab9683fc78cc7c805
Anirudh Raghavendra <araghave@qti.qualcomm.com>
2026-02-27 11:39:59 -0800

    dsp-kernel: Store uid of the current process in user struct
    Query the uid of the current running process and store it in the user
    structure. This field will be used to make scheduling decisions.
    
    Change-Id: Ia219f92dca4b75477ed484457556f1fe125d27d6
    Signed-off-by: Anirudh Raghavendra <araghave@qti.qualcomm.com>

--- dsp/fastrpc_shared.h
+++ dsp/fastrpc_shared.h
@@ -1427,6 +1427,8 @@ struct fastrpc_user {
 	int tgid_frpc;
 	/* Actual hlos pid of process offloading to dsp */
 	int tgid_app;
+	/* UID of process offloading to dsp */
+	uid_t uid;
 	/* Process name of process offloading to dsp */
 	char name[TASK_COMM_LEN];
 	/* PD type of remote subsystem process */
